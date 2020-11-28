2f4ead13bebfc2d9949e468755b72ce85a8bfeb0
TheCaffienatedOne <thecaffinatedone@proton.com>
2020-11-27 20:58:25 -0500

    bruhmoment

---
+++ script.js
@@ -0,0 +1,5 @@
+if (window.screen.height => window.screem.width) {
+  document.getElementById("banner").src = "banner2.png";
+} else if(window.screen.width => window screen.height) {
+  document.getElementById("banner").src = "banner.png";
+}
